2d26ac7c373af40643ba44d467d1c541e399b616
Meanderthal00 <meanderthal00@yahoo.com>
2017-11-27 19:38:55 -0500

    reworking arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,10 @@
 $(document).ready(function () {
     // making sure my JS is linked to html
-    console.log('hi!');
+    // console.log('hi!');
     // ARRAYS
     // questions (5-7)  
     // answers (4 for each question, one correct and 3 bluffs)
-    var questionArray = [{
-        question1: {
-            "If you order 'Escamole' tacos while in Mexico what will your server bring you?",
-            choices1: {
-                ["Tacos smothered in a tangy sauce",
-                    "Tacos filled with ant larve",
-                    "Tacos made with fish eggs",
-                    "Tacos filled with fermented cactus paddles"
-                ],
-                correctAnswer1: {
-                    1
-                }
-            }
-        }
-
-    }, {
-        question2: {
-            "What does the Ukranian dish 'Salo' consist of?",
-            choices2: {
-                [
-                    "Beets and sour cream",
-                    "Chicken stuffed with dried fish",
-                    "Beef and pork patties swimming in vinegar",
-                    "Aged slabs of sliced on rye bread"
-                ],
-                correctAnswer2: {
-                    3
-                    // [3]
-                }
-            }
-        }
-    }, {
-        question3: {
-            "What is the secret ingredient in the Korean dish 'Boshitang'?",
-            choices3: {
-                [
-                    "Lots of Kimchi",
-                    "The eye of a young cow",
-                    "The meat of a small dog",
-                    "The mold from old soy beans"
-                ],
-                correctAnswer3: {
-                    2
-                    // [2]
-                }
-            }
-        }
-
-    }, {
-        question4: {
-            "What surprising part of the Moose do the Canadians turn into a jelly?",
-            choices4: {
-                [
-                    "The 'knuckle'.",
-                    "The antlers",
-                    "The nose",
-                    "The heart"
-                ],
-                correctAnswer4: {
-                    2
-                    // [2]
-                }
-            }
-        }
-    }, {
-        question5: {
-            "What insect first eaten under duress, has become a staple in the diet of many Cambodians?",
-            choices5: {
-                [
-                    "Deep-fried tarantulas",
-                    "Smoked horse-flies",
-                    "Roasted Grasshoppers",
-                    "Grilled earth-worms"
-                ],
-                correctAnswer5: {
-                    0
-                    // [0]
-                }
-            }
-        }
-    }];
+   
 
 
     // $("#questionDisplay").replaceWith(questionArray.question)
@@ -105,7 +25,7 @@ $(document).ready(function () {
 
     // document.getElementById("questionDisplay").innerHTML =questionArray[0];
 
-    
+
     // need a start button for game
     // like to put this in the questionDisplay initially
     // question is displayed
@@ -163,3 +83,93 @@ $(document).ready(function () {
         
     }
 });
+
+
+var counter = 0;
+$(document).on("click", "#start", function(){
+    console.log("HELLO");
+    counter ++;
+    $("#questionDisplay").html(questionArray[counter].question);
+},
+     questionArray = [{
+        question1: {
+            question:
+            "If you order 'Escamole' tacos while in Mexico what will your server bring you?",
+            choices1: {
+                choicesArray:
+                ["Tacos smothered in a tangy sauce",
+                    "Tacos filled with ant larve",
+                    "Tacos made with fish eggs",
+                    "Tacos filled with fermented cactus paddles"
+                ],
+                correctAnswer1: 
+                    1
+                
+            }
+        }
+
+    }, {
+        question2: {
+            question:
+            "What does the Ukranian dish 'Salo' consist of?",
+            choices2: {
+                choicesArray:
+                ["Beets and sour cream",
+                    "Chicken stuffed with dried fish",
+                    "Beef and pork patties swimming in vinegar",
+                    "Aged slabs of sliced on rye bread"
+                ],
+                correctAnswer2: 
+                    3
+                    // [3]
+                }
+            }
+        },{
+    
+        question3: {
+            question:
+            "What is the secret ingredient in the Korean dish 'Boshitang'?",
+            choices3: {
+                choicesArray:
+                [
+                    "Lots of Kimchi",
+                    "The eye of a young cow",
+                    "The meat of a small dog",
+                    "The mold from old soy beans"
+                ],
+                correctAnswer3: 
+                    2
+                    // [2]
+                }
+            }
+        
+
+    }, {
+        question4: {
+            question: "What surprising part of the Moose do the Canadians turn into a jelly?",
+            choicesArray: [
+                    "The 'knuckle'.",
+                    "The antlers",
+                    "The nose",
+                    "The heart"
+                ],
+                correctAnswer4: 2
+                    // [2]
+                } 
+            },
+         {       
+                question5: {
+                    question: "What insect first eaten under duress, has become a staple in the diet of many Cambodians?",
+                    choicesArray: 
+                        [
+                            "Deep-fried tarantulas",
+                            "Smoked horse-flies",
+                            "Roasted Grasshoppers",
+                            "Grilled earth-worms"
+                        ],
+                        correctAnswer5:
+                            0
+                            // [0]
+            }
+        }
+]);
